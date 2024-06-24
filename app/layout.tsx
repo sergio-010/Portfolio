0e51d40a90481c786ad76ecdee9cb36e49f8f28b
sergio-010 <piconsergio010@gmail.com>
2024-06-24 16:46:04 -0500

    nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={urbanist.className}>{children}</body>
+      <body className={urbanist.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
