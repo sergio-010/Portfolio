bab7f025e419a38adccf7c3c4b4c018497cb96d6
sergio-010 <piconsergio010@gmail.com>
2024-06-26 17:24:11 -0500

    fix bavbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
         <TrasitionComponent position="right" className="w-full mt-auto h-max fixed z-40 flex flex-col
          items-center justify-center bottom-10">
             <nav>
-                <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full cursor-pointer bg-white/15 background-blur-sm">
+                <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full cursor-pointer bg-black/50 background-blur-sm">
                     {itemsNavbar.map((item) => (
                         <div key={item.id} className={`px-3 py-2 transition duration-150
                              rounded-full hover:bg-white/10 ${router === item.link ? 'bg-blue-600' : ''}`}>
